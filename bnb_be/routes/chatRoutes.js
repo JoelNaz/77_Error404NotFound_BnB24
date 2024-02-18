13751ef7aa3fe8a563e278143f8fcf035dd0c6c5
unknown <joel.nazareth22@gmail.com>
2024-02-18 11:30:02 +0530

    chat be done 3

--- bnb_be/routes/chatRoutes.js
+++ bnb_be/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Chat } = require('../models/Chat');
+const { Chat } = require('../models/User');
 
 // Endpoint to send a chat message
 router.post('/chat/send', async (req, res) => {
